57dffbef8c175661d2cfab3a66daf9ed233d64ba
Pasquay <22101372@usc.edu.ph>
2025-08-30 10:24:39 +0800

    fix: [Migration] - Remove default value from bio column in users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->text('bio', 500)->nullable()->default('Welcome to my profile!');
+            $table->text('bio', 500)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
