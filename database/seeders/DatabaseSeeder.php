a67419804d69e371006a15ca342d48ec63ee1f06
Pasquay <22101372@usc.edu.ph>
2025-09-04 12:18:45 +0800

    feat: [DatabaseSeeder] - Add DefaultUserSeed and DefaultGroupSeed for initial data seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,11 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $this->call([
+            DefaultUserSeed::class,
+            DefaultGroupSeed::class,
+            DefaultPostSeed::class,
+        ]);
     }
 }
---
+++ database/seeders/DefaultUserSeed.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class DefaultUserSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::firstOrCreate(
+            ['email' => 'admin@usc.edu.ph'],
+            [
+                'name' => 'admin',
+                'password' => bcrypt('admin'),
+                'role' => 'staff',
+            ]
+        );
+    }
+}
